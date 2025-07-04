9cc307a334fc1d36f242813e340098cc6cc59d94
Xinzhu99 <jxinzhu@gmail.com>
2025-07-04 16:05:10 +0200

    counter fonctionnel, stockage local fonctionnel

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const quoteForm = document.querySelector("#quoteForm")
-const textInput = document.querySelector("#textInput")
-const authorInput = document.querySelector("#authorInput")
-const quoteList = document.querySelector("#quoteList")
+const quoteForm = document.querySelector("#quoteForm");
+const textInput = document.querySelector("#textInput");
+const authorInput = document.querySelector("#authorInput");
+const quoteList = document.querySelector("#quoteList");
+const submitBtn = document.querySelector("#submitBtn");
+const text = document.querySelector(".text");
+const count = document.querySelector("#count");
 
-function addQuote(event){
+let quoteCount = 0;
+let quotes = [{author:"", text:""}];
 
-    event.preventDefault()
+quoteForm.addEventListener("submit", (event) => {                      //"submit" pour écouter submission du formulaire en tant que l'event au lieu d'un clic
+    quotes.push({author:authorInput.value,text:textInput.value});
     
-    const quoteText =document.createElement("p")
-    quoteText.className = "text"
-    const quoteAuthor= document.createElement("p")
-    quoteAuthor.className = "author"
-    const quoteZone =document.createElement("div")
-    quoteZone.className = "quote"
-    quoteText.innerText = textInput.value
-    quoteAuthor.innerText = authorInput.value
-    quoteZone.appendChild(quoteText)
-    quoteZone.appendChild(quoteAuthor)
-    quoteList.appendChild(quoteZone)
-}
+    event.preventDefault();                                           //je bloque la recharge de page automatique afin d'afficher le quoteList.innertext
+    quoteList.innerHTML ="";                                          //avant de recharger le nouveau tableau, je vide mon navigateur
+    for (const item of quotes){
+        quoteList.innerHTML += 
+        `<div>
+            <p class="text">${item.text}</p>
+            <p class="author">${item.author}</p>
+        </div>`;
+    };
 
-quoteForm.addEventListener("submit", addQuote)
+    quoteCount ++;                      
+    count.innerHTML=`${quoteCount} citations`;
+
+    textInput.value='';
+    authorInput.value ='';                                            //après chaque clic, je vide mes champs de texte
+});
